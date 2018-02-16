5aaabb324a4153baa671c31f5f4990304494e95e
Cato <carlosarizola17@gmail.com>
2018-02-16 17:50:06 -0500

    Pasar datos a otra activity

--- app/src/main/java/carlos/com/bienvenida/Registrado.java
+++ app/src/main/java/carlos/com/bienvenida/Registrado.java
@@ -2,12 +2,20 @@ package carlos.com.bienvenida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Registrado extends AppCompatActivity {
-
+    TextView mensaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrado);
+        mensaje=(TextView)findViewById(R.id.mensaje);
+    }
+
+    public void MostrarBienvenida(){
+        Bundle datos=getIntent().getExtras();
+        String saludo=datos.getString("usuario");
+        mensaje.setText("Bienvenidozzz"+saludo);
     }
 }
